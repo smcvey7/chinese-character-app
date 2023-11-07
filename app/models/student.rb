ff1c7679c278b417afdd158af93c418f873d8298
Steven McVey <smcvey1210@gmail.com>
2023-11-07 10:39:45 -0500

    add email, username, and password validations for new users

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,10 +6,12 @@ class Student < ApplicationRecord
 
   has_secure_password
 
-  validates :username, {presence: true, uniqueness: true}
-  validates :email, {presence: true, uniqueness: true}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :username, {presence: true, uniqueness: true, format: { with: /\A[a-z0-9\-_.]{5,15}\z/, message: "must be between 5-15 characters and may not contain special characters" }}
+  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, {presence: true, length: {minimum: 6}, allow_blank: true}
-  validates :password_confirmation, {presence: true, allow_blank: true}
+  validates :password, {presence: true, length: {minimum: 6}, format: { with: /\A[a-zA-Z0-9!$#%^&*?\/\-_.+]{6,15}\z/, message: "must be between 6-15 characters and may only contain lowercase and capital letters, numbers, and !$#%^&*?/-_+." }, allow_blank: true}
+  validates :password_confirmation, {presence: true, length: {minimum: 6}, format: { with: /\A[a-zA-Z0-9!$#%^&*?\/\-_.+]{6,15}\z/, message: "must be between 6-15 characters and may only contain lowercase and capital letters, numbers, and !$#%^&*?/-_+." }, allow_blank: true}
 end
