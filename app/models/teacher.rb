5c6f04067c42a0c95e50937ac9ad85ede050e9c7
Steven McVey <smcvey1210@gmail.com>
2023-11-07 10:44:03 -0500

    fix issue with email validation in teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -9,7 +9,7 @@ class Teacher < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :username, {presence: true, uniqueness: true, format: { with: /\A[a-z0-9\-_.]{5,15}\z/, message: "must be between 5-15 characters and may not contain special characters" }}
-  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}  validates :email, {presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }}
+  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, {presence: true, length: {minimum: 6}, format: { with: /\A[a-zA-Z0-9!$#%^&*?\/\-_.+]{6,15}\z/, message: "must be between 6-15 characters and may only contain lowercase and capital letters, numbers, and !$#%^&*?/-_+." }, allow_blank: true}
